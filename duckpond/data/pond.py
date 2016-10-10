6e2b3492cfbb0298277b1abf8d9568cbaa15a6f2
XML Geek; runs with scissors <alex@milowski.com>
2016-10-10 13:27:21 -0700

    Refactored SPARQL generation to allow for graphs various tweaks to the API to make it more uniform

--- duckpond/data/pond.py
+++ duckpond/data/pond.py
@@ -2,6 +2,23 @@ import requests
 import json,io
 from enum import Enum
 from .util import relative
+import urllib
+from .sparql import SPARQL
+
+def strip(value):
+   if value is None:
+      return value
+   elif value[0]=='"':
+      value = value[1:-1]
+      if value[0]=='<' and value[-1]=='>':
+         return value[1:-1]
+      else:
+         return value
+   elif value[0]=='<':
+      return value[1:-1]
+   else:
+      return value
+
 
 class Pond:
 
@@ -14,17 +31,25 @@ class Pond:
       stream = 2
       local = 3
 
-   def __init__(self,service,cache=None):
+   def __init__(self,service,cache=None,facets=None,graphs=None):
       self.service = service
       self.cache = cache
       self.serviceAuth = self.authenticate("anonymous")
       self.resourceAuth = None
       self.prefixes = { 'schema' : 'http://schema.org/'}
+      self.graphs = []
+      if graphs is not None:
+         for graph in graphs:
+            self.graphs.append(graph)
       self.facets = { 'type' : 'schema:BlogPosting',
            'order' : 'schema:datePublished',
-           'summary' : 'schema:headline',
+           'title' : 'schema:headline',
+           'summary' : 'schema:description',
            'resource' : 'schema:isBasedOnUrl',
            'category' : 'schema:keywords'}
+      if facets is not None:
+         for key in facets.keys():
+            self.facets[key] = facet[key]
 
    def getProlog(self):
       glob = io.StringIO()
@@ -33,7 +58,7 @@ class Pond:
       return glob.getvalue()
 
    def setProxyCache(self,proxy,base):
-      this.cache = { 'proxy': proxy, 'base':base }
+      self.cache = { 'proxy': proxy, 'base':base }
 
    def authenticate(self,user,password="",method=AuthMethod.basic):
       if (method is Pond.AuthMethod.basic):
@@ -43,35 +68,58 @@ class Pond:
       else:
          raise Exception("Unsupported authentication method")
 
+   def defaultParameters(self):
+      params = {}
+      return params
+
+   def mergeParameters(self,requestParameters):
+      params = self.defaultParameters()
+      for param in requestParameters.keys():
+         params[param] = requestParameters[param]
+      return params
+
+   def getGraphs(self):
+      named = ""
+      for graph in self.graphs:
+         named = named + "from named <" + graph +"> "
+      return named
+
    def facet(self,name,value = None):
       return value if value is not None else self.facets[name]
 
-   def currentEntity(self,entity = None,order = None,summary = None, resource = None):
+   def currentEntity(self,entity = None,order = None,title = None, summary = None, resource = None):
 
       entityType = self.facet('type',entity)
       orderFacet = self.facet('order',order)
+      titleFacet = self.facet('title',title)
       summaryFacet = self.facet('summary',summary)
       resourceFacet = self.facet('resource',resource)
 
-      query = self.getProlog() + """
-select ?s ?summary ?ordering ?basedOnUrl where {{ ?s rdf:type {0}; {1} ?ordering; {2} ?summary; {3} ?basedOnUrl }} order by desc(?date)
-""".format(entityType,orderFacet,summaryFacet,resourceFacet)
-      print(query)
-      params = {'limit':2,'query':query}
+      expr = '?s rdf:type {0}; {1} ?ordering; {2} ?title; {4} ?basedOnUrl . optional {{ ?s {3} ?summary . }}'.format(entityType,orderFacet,titleFacet,summaryFacet,resourceFacet)
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(["s","title","summary","ordering","basedOnUrl"]) \
+            .fromGraphs(self.graphs) \
+            .where(expr) \
+            .orderBy("desc(?ordering)")
+      #print(q)
+      params = self.mergeParameters({'limit':1,'query':str(q)})
 
-      req = requests.get(self.service,params=params,headers={'accept':'application/json,text/html'},auth=self.serviceAuth)
+      req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
       if (req.status_code>=200 or req.status_code<300):
-         print(req.text)
+         #print(req.text)
          data = json.loads(req.text)
          if len(data['values']) == 0:
-            return (None,None,None,None,None,{})
-         subject = data['values'][0][0][1:-1]
-         summary = data['values'][0][1][1:-1]
-         dateTime = data['values'][0][2][1:-1]
-         basedOnUrl = data['values'][0][3][1:-1]
+            return (None,None,None,None,None,None)
+         subject = strip(data['values'][0][0])
+         title = strip(data['values'][0][1])
+         summary = strip(data['values'][0][2])
+         dateTime = strip(data['values'][0][3])
+         basedOnUrl = strip(data['values'][0][4])
          date,time = dateTime.split('T') # Need to check that is xsd:dateTime
-         return (subject,summary,date,time,basedOnUrl,{'uri':relative(data['values'][1][0][1:-1]),'summary':data['values'][1][1][1:-1]} if len(data['values'])>1 else None)
+         #{'uri':relative(strip(data['values'][1][0])),'title':strip(data['values'][1][1]),'summary':strip(data['values'][1][2])} if len(data['values'])>1 else None
+         return (subject,title,summary,date,time,basedOnUrl)
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
 
@@ -110,72 +158,81 @@ select ?s ?summary ?ordering ?basedOnUrl where {{ ?s rdf:type {0}; {1} ?ordering
 
       if self.cache!=None and 'redirect' in self.cache and self.cache['redirect']:
          return (Pond.ResourceType.uri,uri,303)
-      if cache==None or uri[0:5]=='http:' or uri[0:6]=='https:':
+      if self.cache==None or uri[0:5]=='http:' or uri[0:6]=='https:':
          req = requests.get(uri, stream = True,auth=self.resourceAuth)
          return (Pond.ResourceType.stream,req.iter_content(),req.headers['content-type'])
       elif uri[0:7]=='file://':
          return (Pond.ResourceType.local, uri[7:],None)
+      else:
+         return (Pond.ResourceType.local, uri,None)
 
-   def relatedEntityByOrder(self,value,previous = True,entity = None,order = None,summary = None):
+   def relatedEntityByOrder(self,value,previous = True,entity = None,order = None,title = None,summary = None):
       entityType = self.facet('type',entity)
       orderFacet = self.facet('order',order)
+      titleFacet = self.facet('title',title)
       summaryFacet = self.facet('summary',summary)
-      query = self.getProlog() + """
-select ?s ?summary ?ordering
-where {{
-   ?s rdf:type {0}; {1} ?ordering; {2} ?summary .
-   FILTER( ?ordering """ + ('>' if previous else '<') + ' "' + value + '"' + """ )
-}}
-order by """
-      query = query.format(entityType,orderFacet,summaryFacet) + ('?ordering' if previous else 'desc(?ordering)')
-      print(query)
-      params = {'limit':1,'query':query}
+      expr = '?s rdf:type {0}; {1} ?ordering; {2} ?title; . optional {{ ?s {3} ?summary . }}'.format(entityType,orderFacet,titleFacet,summaryFacet) \
+             + ' FILTER( ?ordering ' + ('>' if previous else '<') + ' "' + value + '"' + ' )'
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(["s","title","summary","ordering","basedOnUrl"]) \
+            .fromGraphs(self.graphs) \
+            .where(expr ) \
+            .orderBy("desc(?ordering)")
+      #print(q)
+      params = self.mergeParameters({'limit':1,'query':str(q)})
 
       req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
       if (req.status_code>=200 or req.status_code<300):
          data = json.loads(req.text)
-         return {'uri':relative(data['values'][0][0][1:-1]),'summary':data['values'][0][1][1:-1]} if len(data['values'])>0 else None
+         values = data['values']
+         return {'uri':relative(strip(values[0][0])),'title':strip(values[0][1]),'summary':strip(values[0][2]) } if len(values)>0 else None
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
 
-   def entityByOrder(self,ordering,entity = None,order = None,summary = None,resource = None):
+   def entityByOrder(self,ordering,entity = None,order = None,title = None,summary = None,resource = None):
       entityType = self.facet('type',entity)
       orderFacet = self.facet('order',order)
+      titleFacet = self.facet('title',title)
       summaryFacet = self.facet('summary',summary)
       resourceFacet = self.facet('resource',resource)
-      query = self.getProlog() + """
-select ?s ?headline ?isBasedOnUrl
-where {{
-   ?s rdf:type {0}; {1} """ + '"' + ordering + '"' + """; {2} ?headline ; {3} ?isBasedOnUrl.
-}}
-"""
-      query = query.format(entityType,orderFacet,summaryFacet,resourceFacet)
-      params = {'limit':1,'query':query}
+      expr = '?s rdf:type {0}; {1} "{2}"; {3} ?title; {5} ?basedOnUrl . optional {{ ?s {4} ?summary . }}'.format(entityType,orderFacet,ordering,titleFacet,summaryFacet,resourceFacet)
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(["s","title","summary","basedOnUrl"]) \
+            .fromGraphs(self.graphs) \
+            .where(expr)
+      #print(q)
+      params = self.mergeParameters({'limit':1,'query':str(q)})
 
       req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
       if (req.status_code>=200 or req.status_code<300):
          data = json.loads(req.text)
-         subject = data['values'][0][0][1:-1]
-         headline = data['values'][0][1][1:-1]
-         basedOn = data['values'][0][2][1:-1]
-         return (subject,headline,basedOn) if len(data['values'])>0 else (None,None,None)
+         values = data['values']
+         subject = strip(values[0][0])
+         title = strip(values[0][1])
+         summary = strip(values[0][2])
+         basedOnUrl = strip(values[0][3])
+         date,time = ordering.split('T') # Need to check that is xsd:dateTime
+         return (subject,title,summary,date,time,basedOnUrl)
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
 
    def getCategoryCount(self,entity = None,category = None):
       entityType = self.facet('type',entity)
       categoryFacet = self.facet('category',category)
-      query = self.getProlog() + """
-select ?category (count(?category) as ?count)
-where {{
-     ?s rdf:type {0}; {1} ?category
-}}
-group by ?category
-order by desc(?count)
-""".format(entityType,categoryFacet)
-      params = {'limit':100,'query':query}
+      expr = '?s rdf:type {0}; {1} ?category'.format(entityType,categoryFacet)
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(['category','(count(?category) as ?count)']) \
+            .fromGraphs(self.graphs) \
+            .where(expr) \
+            .groupBy('?category') \
+            .orderBy('desc(?count)')
+      #print(q)
+      params = self.mergeParameters({'limit':100,'query':str(q)})
 
       req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
@@ -184,7 +241,7 @@ order by desc(?count)
          keywords = {}
          for keyword in data['values']:
             value,type = keyword[1].split('^^')
-            keywords[keyword[0][1:-1]] = int(value[1:-1])
+            keywords[strip(keyword[0])] = int(strip(value))
          return keywords
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
@@ -192,41 +249,38 @@ order by desc(?count)
    def getEntityCategories(self,subject,entity = None,category = None):
       entityType = self.facet('type',entity)
       categoryFacet = self.facet('category',category)
-      query = self.getProlog() + """
-select ?category
-where {{
-   <""" + subject +"""> rdf:type {0}; {1} ?category
-}}
-"""
-      query = query.format(entityType,categoryFacet)
-      params = {'limit':100,'query':query}
+      expr = '<{0}> rdf:type {1}; {2} ?category'.format(subject,entityType,categoryFacet)
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(['category']) \
+            .fromGraphs(self.graphs) \
+            .where(expr)
+      params = self.mergeParameters({'limit':100,'query':str(q)})
 
-      print(query)
       req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
       if (req.status_code>=200 or req.status_code<300):
          data = json.loads(req.text)
          keywords = []
          for keyword in data['values']:
-            keywords.append(keyword[0][1:-1])
+            keywords.append(strip(keyword[0]))
          return keywords
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
 
-   def getEntitiesByCategory(self,value,limit = 100,entity = None,category = None,summary = None,order = None):
+   def getEntitiesByCategory(self,value,limit = 100,entity = None,category = None,title = None,summary = None,order = None):
       entityType = self.facet('type',entity)
       categoryFacet = self.facet('category',category)
+      titleFacet = self.facet('title',title)
       summaryFacet = self.facet('summary',summary)
       orderFacet = self.facet('order',order)
-      query = self.getProlog() + """
-select ?s ?date ?headline
-where {{
-   ?s rdf:type {0}; {1} ?headline; {2} ?date; {3} """ + '"' + value + '"' """
-}}
-"""
-      query = query.format(entityType,summaryFacet,orderFacet,categoryFacet)
-      print(query)
-      params = {'limit':limit,'query':query}
+      expr = '?s rdf:type {0}; {1} ?title; {3} ?date; {4} "{5}" . optional {{ ?s {2} ?summary; }}'.format(entityType,titleFacet,summaryFacet,orderFacet,categoryFacet,value)
+      q = SPARQL() \
+            .start(self.prefixes) \
+            .select(['s','date','title','summary']) \
+            .fromGraphs(self.graphs) \
+            .where(expr)
+      params = self.mergeParameters({'limit':limit,'query':str(q)})
 
       req = requests.get(self.service,params=params,headers={'accept':'application/json'},auth=self.serviceAuth)
 
@@ -234,7 +288,7 @@ where {{
          data = json.loads(req.text)
          entries = []
          for entry in data['values']:
-            entries.append({'subject': entry[0][1:-1], 'uri': relative(entry[0][1:-1]), 'datePublished' : entry[1][1:-1], 'summary' : entry[2][1:-1]})
+            entries.append({'subject': strip(entry[0]), 'uri': relative(strip(entry[0])), 'datePublished' : strip(entry[1]), 'title' : strip(entry[2]), 'summary' : strip(entry[3])})
          return entries
       else:
          raise IOError('Cannot post data to uri <{}>, status={}'.format(self.service,req.status_code))
---
+++ duckpond/data/sparql.py
@@ -0,0 +1,80 @@
+import io
+
+class SPARQL:
+
+   def __init__(self):
+      self.statement = io.StringIO()
+
+   def start(self,prefixes):
+      for key in prefixes:
+         self.statement.write('prefix {}: <{}>\n'.format(key,prefixes[key]))
+      return self
+
+   def select(self,names):
+      self.statement.write('select ')
+      for name in names:
+         if name[0]=='(':
+            self.statement.write(name)
+         else:
+            self.statement.write('?')
+            self.statement.write(name)
+         self.statement.write(' ')
+      self.statement.write('\n')
+      return self
+
+   def fromGraphs(self,graphs):
+      for graph in graphs:
+         self.statement.write('from <{0}>\n'.format(graph))
+      return self
+
+   def fromNameGraphs(self,graphs):
+      for graph in graphs:
+         self.statement.write('from named <{0}>\n'.format(graph))
+      return self
+
+   def where(self,*expressions):
+      self.statement.write('where {')
+      for expression in expressions:
+         self.statement.write(str(expression))
+         self.statement.write('\n')
+      self.statement.write('}\n')
+      return self
+
+   def anyGraph(self,*expressions):
+      self.statement.write('graph ?g {')
+      for expression in expressions:
+         self.statement.write(str(expression))
+         self.statement.write('\n')
+      self.statement.write('}\n')
+      return self
+
+   def forGraph(self,graph,*expressions):
+      self.statement.write('graph <{0}> {'.format(graph))
+      for expression in expressions:
+         self.statement.write(str(expression))
+         self.statement.write('\n')
+      self.statement.write('}\n')
+      return self
+
+   def newline(self):
+      self.statement.write('\n')
+      return self;
+
+   def orderBy(self,*expressions):
+      self.statement.write('order by ')
+      first = True
+      for expression in expressions:
+         if not first:
+            self.statement.write(',')
+         self.statement.write(str(expression))
+      self.statement.write('\n')
+      return self
+
+   def groupBy(self,expression):
+      self.statement.write('group by ')
+      self.statement.write(expression)
+      self.statement.write('\n')
+      return self
+
+   def __str__(self):
+      return self.statement.getvalue()
