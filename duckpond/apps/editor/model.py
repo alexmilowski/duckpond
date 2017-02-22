e4a4ac9edd21855c2774ebff778b35f97548076c
Alex Milowski <alex@milowski.com>
2017-02-22 08:00:50 -0800

    Fix for editor service proxy close issue

--- duckpond/apps/editor/model.py
+++ duckpond/apps/editor/model.py
@@ -11,7 +11,7 @@ def getAuth():
 
 def getContentList():
    url = app.config['SERVICE'] + 'content/'
-   req = requests.get(url,auth=getAuth())
+   req = requests.get(url,auth=getAuth(),headers={'Connection' : 'close'})
    if req.status_code!=200:
       raise IOError('Cannot get content list from service, status {status}'.format(**{'status':req.status_code}))
    return json.loads(req.text)
@@ -19,7 +19,7 @@ def getContentList():
 def createContent(typeName,genre,name,headline):
    url = app.config['SERVICE'] + 'content/'
    user = app.config['AUTH_SERVICE'].getUser(session['username'])
-   headers = {'Content-Type' : 'application/ld+json'}
+   headers = {'Content-Type' : 'application/ld+json', 'Connection' : 'close'}
    data = {
       '@context' : 'http://schema.org/',
       '@type' : typeName,
@@ -35,12 +35,12 @@ def createContent(typeName,genre,name,headline):
 
 def deleteContent(id):
    url = app.config['SERVICE'] + 'content/' + id + '/'
-   response = requests.delete(url,auth=getAuth())
+   response = requests.delete(url,auth=getAuth(),headers={'Connection' : 'close'})
    return response.status_code
 
 def getContent(id):
    url = app.config['SERVICE'] + 'content/' + id + '/'
-   response = requests.get(url,auth=getAuth())
+   response = requests.get(url,auth=getAuth(),headers={'Connection' : 'close'})
    if response.status_code==200:
       return json.loads(response.text)
    else:
@@ -48,13 +48,13 @@ def getContent(id):
 
 def updateContent(id,data):
    url = app.config['SERVICE'] + 'content/' + id + '/'
-   headers = {'Content-Type' : 'application/ld+json'}
+   headers = {'Content-Type' : 'application/ld+json', 'Connection' : 'close'}
    response = requests.put(url,auth=getAuth(),data=json.dumps(data),headers=headers)
    return (response.status_code,response.iter_content(chunk_size=10*1024),response.headers.get('content-type'))
 
 def getContentResource(id,name):
    url = app.config['SERVICE'] + 'content/' + id + '/' + name
-   response = requests.get(url,auth=getAuth(),stream = True)
+   response = requests.get(url,auth=getAuth(),stream = True,headers={'Connection' : 'close'})
    return (response.status_code,response.iter_content(chunk_size=10*1024),response.headers.get('content-type'))
 
 def updateContentResource(id,name,content_type,data):
@@ -63,17 +63,17 @@ def updateContentResource(id,name,content_type,data):
       shutil.copyfileobj(data,cached)
       size = cached.tell()
       cached.seek(0)
-      headers = {'Content-Type' : content_type, 'Content-Length' : str(size)}
+      headers = {'Content-Type' : content_type, 'Content-Length' : str(size), 'Connection' : 'close'}
       response = requests.put(url,auth=getAuth(),data=cached,headers=headers)
       return (response.status_code,response.iter_content(chunk_size=10*1024),response.headers.get('content-type'))
 
 def uploadContentResource(id,property,name,content_type,content_length,data):
    url = app.config['SERVICE'] + 'content/' + id + '/' + name + ";" + property
-   headers = {'Content-Type' : content_type, 'Content-Length' : str(content_length)}
+   headers = {'Content-Type' : content_type, 'Content-Length' : str(content_length), 'Connection' : 'close'}
    response = requests.put(url,auth=getAuth(),data=data,headers=headers)
    return (response.status_code,response.iter_content(chunk_size=10*1024),response.headers.get('content-type'))
 
 def deleteContentResource(id,name):
    url = app.config['SERVICE'] + 'content/' + id + '/' + name
-   response = requests.delete(url,auth=getAuth())
+   response = requests.delete(url,auth=getAuth(),headers={'Connection' : 'close'})
    return response.status_code
